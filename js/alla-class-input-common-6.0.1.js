267620ebbb3b33b79068f8f8a957f025514517b8
unknown <allaclass21@gmail.com>
2024-04-03 23:17:56 +0900

    [Feat] 문제 분리 함수 개발중

--- js/alla-class-input-common-6.0.1.js
+++ js/alla-class-input-common-6.0.1.js
@@ -7,9 +7,9 @@
 // ------------------------------------------
 
 // 1. 텍스트박스 주소값 지정
-let txtBoxContent = document.getElementById('contents');
-let txtBoxResult = document.getElementById('result');
-let txtBoxFinal = document.getElementById('final');
+let txtBoxInputData = document.getElementById('txtBoxInputData');
+let txtBoxOutputData = document.getElementById('txtBoxOutputData');
+let txtBoxOutputHtml = document.getElementById('txtBoxOutputHtml');
 let txtBoxInfo = document.getElementById('txt_info');
 let txtBoxTitle = document.getElementById('txt_title');
 let txtBoxTag = document.getElementById('txt_tag');
@@ -57,8 +57,7 @@ let toHTML_NoticeBottom = '';
 // 1. 함수 선언
 // 1-1. 타이틀정보 가이드라인 초기값 입력
 let fnTxtBoxTitle = () => {
-  txtBoxInfo.value =
-    '[과목명] \n[종  류] \n[연  도] \n[학  기] \n[학  년] \n[교수명] \n[셤범위] \n[시작넘] \n[종료넘] \n[썸네일] \n[정  답] ';
+  txtBoxInfo.value = '[과목명] \n[종  류] \n[연  도] \n[학  기] \n[학  년] \n[교수명] \n[셤범위] \n[시작넘] \n[종료넘] \n[썸네일] \n[정  답] ';
 };
 
 // 2. 함수 호출
@@ -86,55 +85,18 @@ let fnQuestionInfo = () => {
   let infoIndexAnswer = infoDataAll.indexOf('[정  답]');
   let infoIndexLength = infoDataAll.length;
   // 1-3. 내용만 발라내서 변수에 할당
-  // 1-3-1. 과목명
-  infoDataObject = infoDataAll
-    .substring(infoIndexObject + 5, infoIndexKind)
-    .replace(/(^\s*)|(\s*$)/gi, '');
-  // 1-3-2. 종류
-  infoDataKind = infoDataAll
-    .substring(infoIndexKind + 6, infoIndexYear)
-    .replace(/(^\s*)|(\s*$)/gi, '');
-  // 1-3-3. 연도
-  infoDataYear = infoDataAll
-    .substring(infoIndexYear + 6, infoIndexSeme)
-    .replace(/(^\s*)|(\s*$)/gi, '');
-  // 1-3-4. 학기
-  infoDataSeme = infoDataAll
-    .substring(infoIndexSeme + 6, infoIndexGrade)
-    .replace(/(^\s*)|(\s*$)/gi, '');
-  // 1-3-5. 학년
-  infoDataGrade = infoDataAll
-    .substring(infoIndexGrade + 6, infoIndexProf)
-    .replace(/(^\s*)|(\s*$)/gi, '');
-  // 1-3-6. 교수명
-  infoDataProf = infoDataAll
-    .substring(infoIndexProf + 5, infoIndexRange)
-    .replace(/(^\s*)|(\s*$)/gi, '');
-  // 1-3-7. 셤범위
-  infoDataRange = infoDataAll
-    .substring(infoIndexRange + 5, infoIndexStartNum)
-    .replace(/(^\s*)|(\s*$)/gi, '');
-  // 1-3-8. 문제 시작 번호
-  infoDataStartNum = Number(
-    infoDataAll
-      .substring(infoIndexStartNum + 5, infoIndexEndNum)
-      .replace(/(^\s*)|(\s*$)/gi, '')
-  );
-  // 1-3-9. 문제 종료 번호
-  infoDataEndNum = Number(
-    infoDataAll
-      .substring(infoIndexEndNum + 5, infoIndexThumb)
-      .replace(/(^\s*)|(\s*$)/gi, '')
-  );
-  // 1-3-10. 썸네일
-  infoDataCountNum = infoDataEndNum - infoDataStartNum + 1;
-  infoDataThumb = infoDataAll
-    .substring(infoIndexThumb + 5, infoIndexAnswer)
-    .replace(/(^\s*)|(\s*$)/gi, '');
-  // 1-3-11. 정답
-  infoDataAnswer = infoDataAll
-    .substring(infoIndexAnswer + 6, infoIndexLength)
-    .replace(/(^\s*)|(\s*$)/gi, '');
+  infoDataObject = infoDataAll.substring(infoIndexObject + 5, infoIndexKind).replace(/(^\s*)|(\s*$)/gi, ''); // 1-3-1. 과목명
+  infoDataKind = infoDataAll.substring(infoIndexKind + 6, infoIndexYear).replace(/(^\s*)|(\s*$)/gi, ''); // 1-3-2. 종류
+  infoDataYear = infoDataAll.substring(infoIndexYear + 6, infoIndexSeme).replace(/(^\s*)|(\s*$)/gi, ''); // 1-3-3. 연도
+  infoDataSeme = infoDataAll.substring(infoIndexSeme + 6, infoIndexGrade).replace(/(^\s*)|(\s*$)/gi, ''); // 1-3-4. 학기
+  infoDataGrade = infoDataAll.substring(infoIndexGrade + 6, infoIndexProf).replace(/(^\s*)|(\s*$)/gi, ''); // 1-3-5. 학년
+  infoDataProf = infoDataAll.substring(infoIndexProf + 5, infoIndexRange).replace(/(^\s*)|(\s*$)/gi, ''); // 1-3-6. 교수명
+  infoDataRange = infoDataAll.substring(infoIndexRange + 5, infoIndexStartNum).replace(/(^\s*)|(\s*$)/gi, ''); // 1-3-7. 셤범위
+  infoDataStartNum = Number(infoDataAll.substring(infoIndexStartNum + 5, infoIndexEndNum).replace(/(^\s*)|(\s*$)/gi, '')); // 1-3-8. 문제 시작 번호
+  infoDataEndNum = Number(infoDataAll.substring(infoIndexEndNum + 5, infoIndexThumb).replace(/(^\s*)|(\s*$)/gi, '')); // 1-3-9. 문제 종료 번호
+  infoDataCountNum = infoDataEndNum - infoDataStartNum + 1; // 1-3-10. 총문항수
+  infoDataThumb = infoDataAll.substring(infoIndexThumb + 5, infoIndexAnswer).replace(/(^\s*)|(\s*$)/gi, ''); // 1-3-10. 썸네일
+  infoDataAnswer = infoDataAll.substring(infoIndexAnswer + 6, infoIndexLength).replace(/(^\s*)|(\s*$)/gi, ''); // 1-3-11. 정답
 };
 
 // 2. 텍스트박스(타이틀) 자동 작성 함수
@@ -167,10 +129,42 @@ let consolelog = () => {
   infoDataEndNum: ${infoDataEndNum}
   infoDataCountNum: ${infoDataCountNum}
   infoDataThumb: ${infoDataThumb}
-  infoDataAnswer: ${infoDataAnswer}`);
+  infoDataAnswer: ${infoDataAnswer}
+  `);
+};
+
+let lv1 = () => {
+  // let countGlobalNum = 0;
+  let forEnd = 100;
+
+  for (let i = 0; i < forEnd; i++) {
+    let countStartNum = infoDataStartNum + i;
+    let countNextNum = countStartNum + 1;
+    let countEndNum = infoDataEndNum;
+
+    // 문제영역 나누기용 Index 설정
+    let groupQuestionStartIndex = txtBoxInputData.value.indexOf('/' + String(countStartNum) + '/');
+    let groupQuestionNextIndex = txtBoxInputData.value.indexOf('/' + String(countNextNum) + '/');
+    let questionStartIndex = txtBoxInputData.value.indexOf(String(countStartNum) + '.');
+    let questionNextIndex = txtBoxInputData.value.indexOf(String(countNextNum) + '.');
+    let questionEndIndex = txtBoxInputData.value.length;
+
+    // 중간점검 콘솔로그
+    // console.log(`
+    // groupQuestionStartIndex: ${groupQuestionStartIndex}
+    // groupQuestionNextIndex: ${groupQuestionNextIndex}
+    // questionStartIndex: ${questionStartIndex}
+    // questionNextIndex: ${questionNextIndex}
+    // questionEndIndex: ${questionEndIndex}
+    // `);
+
+    // 실행문
+    if (groupQuestionStartIndex >= 0) {
+      // 그룹문제문 유효 시
+    }
+  }
 };
 
 let btn1 = () => {
   fnQuestionInfo(); // 인포데이터 내용 발라내기
-  consolelog();
 };
