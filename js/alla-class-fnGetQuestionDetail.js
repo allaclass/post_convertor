7e29cd4e205abb4bfd91314aee5c524db8d7cfe7
unknown <allaclass21@gmail.com>
2024-04-07 05:26:43 +0900

    [Feat] 보기문 라벨링 기능에 추가보기문 추가보기그림 패턴 추가

--- js/alla-class-fnGetQuestionDetail.js
+++ js/alla-class-fnGetQuestionDetail.js
@@ -7,11 +7,11 @@ let fnGetQuestionDetail = (str, simbol, type) => {
   let setPatterns = (type, simbol) => {
     let arrPatterns = [];
     if (type == 'group') {
-      arrPatterns = [`/보기문`, `/보기그림`];
+      arrPatterns = [`/보기문`, `/추가보기문`, `/보기그림`, `/추가보기그림`];
       arrPatterns.unshift(simbol);
     } else {
       // type == 'normal'
-      arrPatterns = [`/보기문`, `/보기그림`, `①`, `②`, `③`, `④`, `/해설`];
+      arrPatterns = [`/보기문`, `/추가보기문`, `/보기그림`, `/추가보기그림`, `①`, `②`, `③`, `④`, `/해설`];
       arrPatterns.unshift(simbol);
     }
     return arrPatterns;
@@ -28,7 +28,13 @@ let fnGetQuestionDetail = (str, simbol, type) => {
           case '/보기문':
             indices.push({ type: 'example_txt', content: pattern, index: match.index });
             break;
+          case '/추가보기문':
+            indices.push({ type: 'example_txt', content: pattern, index: match.index });
+            break;
           case '/보기그림':
+            indices.push({ type: 'example_img', content: pattern, index: match.index });
+            break;
+          case '/추가보기그림':
             indices.push({ type: 'example_img', content: pattern, index: match.index });
             break;
           case '①':
@@ -79,9 +85,9 @@ let fnGetQuestionDetail = (str, simbol, type) => {
 
       if (i !== indices.length) {
         let item_next_index = indices[i].index;
-        temp = str.substring(item_current_index, item_next_index).trim();
+        temp = str.substring(item_current_index + item_current_content.length, item_next_index).trim();
       } else {
-        temp = str.substring(item_current_index, str.length).trim();
+        temp = str.substring(item_current_index + item_current_content.length, str.length).trim();
       }
 
       if (item_current_type == 'simbol') {
@@ -98,14 +104,14 @@ let fnGetQuestionDetail = (str, simbol, type) => {
     // 예시)
     // { type: 'simbol', content: '1.' }
     // { type: 'question', content: '문제 질문 내용' }
-    // { type: 'example_txt', content: '/보기문\n첫번째 보기문' }
-    // { type: 'example_img', content: '/보기그림\n첫번째 보기그림' }
-    // { type: 'example_txt', content: '/보기문\n두번째 보기문' }
-    // { type: 'select_1', content: '① ㉠' }
-    // { type: 'select_2', content: '② ㉡' }
-    // { type: 'select_3', content: '③ ㉢' }
-    // { type: 'select_4', content: '④ ㉣' }
-    // { type: 'solve', content: '/해설\n해설 내용입니다.' }
+    // { type: 'example_txt', content: '첫번째 보기문' }
+    // { type: 'example_img', content: '첫번째 보기그림' }
+    // { type: 'example_txt', content: '두번째 보기문' }
+    // { type: 'select_1', content: '㉠' }
+    // { type: 'select_2', content: '㉡' }
+    // { type: 'select_3', content: '㉢' }
+    // { type: 'select_4', content: '㉣' }
+    // { type: 'solve', content: '해설 내용입니다.' }
   };
 
   // --------------------------------------------------------------------------------------
@@ -157,7 +163,7 @@ let fnGetQuestionDetail = (str, simbol, type) => {
 // let simbol = `1\\.`;
 // let str = `
 // 1.
-// 문제 질문 내용
+// 문제 질문 내용.
 // /보기문
 // 첫번째 보기문
 // /보기그림
