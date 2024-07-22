5b2a720cd8ab2f8f1f6a2795a03f346d82dcbb46
unknown <allaclass21@gmail.com>
2024-07-22 23:32:02 +0900

    [Design] PostView에 티스토리 이미지 치환자를 이미지로 표현하는 기능 추가

---
+++ ver.2/css-js/fn_wrapRight_Thumb_ImageChange.js
@@ -0,0 +1,20 @@
+// wrapRight > wrapPostView > 티스토리 썸네일 치환자 이미지로 변환하는 기능
+
+// 일본어문장연습 19-1-2
+// https://blog.kakaocdn.net/dn/cE8euR/btsIDNwuNsH/loD6lKL2FFrk3iFtYvVugk/img.png
+// [##_Image|kage@cE8euR/btsIDNwuNsH/loD6lKL2FFrk3iFtYvVugk/img.png|CDM|1.3|{"originWidth":600,"originHeight":600,"style":"alignCenter","alt":"0-썸네일-일본어문장연습-기말-19-1-2","caption":"0-썸네일-일본어문장연습-기말-19-1-2","filename":"0-썸네일-일본어문장연습-기말-19-1-2.png"}_##]
+
+// 일본어문장연습 18-1-2
+// https://blog.kakaocdn.net/dn/H1GaR/btsICUC7VSl/DpOqYZFKlnuEDkFrT0bRk0/img.png
+// [##_Image|kage@H1GaR/btsICUC7VSl/DpOqYZFKlnuEDkFrT0bRk0/img.png|CDM|1.3|{"originWidth":600,"originHeight":600,"style":"alignCenter","alt":"0-썸네일-일본어문장연습-기말-18-1-2","caption":"0-썸네일-일본어문장연습-기말-18-1-2","filename":"0-썸네일-일본어문장연습-기말-18-1-2.png"}_##]
+
+let fn_ToTistoryImageSource_FromTistoryImageCommand = (postViewHtmlSrc) => {
+  let newPostViewHtmlSrc = postViewHtmlSrc.replaceAll('[##_Image|kage@', '<img src="https://blog.kakaocdn.net/dn/');
+  newPostViewHtmlSrc = newPostViewHtmlSrc.replaceAll(/\|CDM\|.*?_##\]/, '">');
+  return newPostViewHtmlSrc;
+};
+
+let fn_thumbnail_width100 = () => {
+  const divThumbImage = document.querySelector('.alla6ThumbDiv img');
+  divThumbImage.style.width = '100%';
+};
